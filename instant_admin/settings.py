0afc54029bf106f42659ac83926bfa86fd86f517
Ville Skyttä <ville.skytta@iki.fi>
2019-04-14 08:50:12 +0300

    settings: use project name in sqlite db filename

--- instant_admin/settings.py
+++ instant_admin/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'instant_admin.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'instant_admin.sqlite3'),
     }
 }
 
