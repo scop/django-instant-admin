aa4e3bba472fd8d6c49a417b6908803c7c30318b
Ville Skyttä <ville.skytta@iki.fi>
2019-04-14 09:25:19 +0300

    settings: add inspected database config

--- instant_admin/settings.py
+++ instant_admin/settings.py
@@ -74,11 +74,14 @@ WSGI_APPLICATION = 'instant_admin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'instant_admin.sqlite3'),
-    }
+    },
+    'inspected': dj_database_url.config(),
 }
 
 
