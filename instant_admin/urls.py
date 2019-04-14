8099a52a2ce5bfb6991436d454d2fae862545217
Ville Skyttä <ville.skytta@iki.fi>
2019-04-14 08:52:02 +0300

    urls: redirect root to admin

--- instant_admin/urls.py
+++ instant_admin/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path
 
 urlpatterns = [
+    path('', lambda r: redirect('admin/')),
     path('admin/', admin.site.urls),
 ]
